95deea0d673e86f8b5deabce7bc73ccbe9256dae
Ben Brandon/ <benbrandon97@gmail.com>
2024-03-10 09:39:15 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+print("Good morning, my name is " + name, "I am " + age, "years old." "I live at " + location, "in Kisumu and I like it here.")
